4fc9a5fb863ebdd25bce640a39865dc03a4665d6
Shivam Sharma <kshivam654@gmail.com>
2019-11-01 12:19:18 +0530

    Finish project for custom popups

---
+++ app/src/main/java/app/sharma/coustompopup/MainActivity.java
@@ -0,0 +1,48 @@
+package app.sharma.coustompopup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    Dialog myDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        myDialog = new Dialog(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txtclose;
+                Button btnFollow;
+                myDialog.setContentView(R.layout.custom_popup);
+                txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+                txtclose.setText("M");
+                btnFollow = (Button) myDialog.findViewById(R.id.btnfollow);
+                txtclose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        myDialog.dismiss();
+                    }
+                });
+                myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                myDialog.show();
+            }
+        });
+    }
+}
